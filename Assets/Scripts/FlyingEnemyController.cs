67783200e49ecb3f4d1cf22039d5ae8d558008ff
Sepehr <51531251+Sepehr-Boz@users.noreply.github.com>
2024-11-18 20:35:56 +0000

    BulkyEnemy + Small Enemy Update added in new enemy variant: bulkyenemy that is melee but slower but stronger. Changed the layer of the Vision gameobject on the enemies to ignore raycasts so now the flying enemy bounces as intended.

--- Assets/Scripts/FlyingEnemyController.cs
+++ Assets/Scripts/FlyingEnemyController.cs
@@ -15,10 +15,8 @@ public class FlyingEnemyController : EnemyController
             GetComponent<Rigidbody2D>().MovePosition((Vector2)transform.position +
             (new Vector2(view.player.transform.position.x - transform.position.x, 0) * moveSpeed * Time.deltaTime));
         }
-        // GetComponent<Rigidbody2D>().linearVelocityX = (view.player.transform.position.x - transform.position.x) * moveSpeed * Time.deltaTime;
         else
         {
-            // GetComponent<Rigidbody2D>().linearVelocityX = moveDir.x * moveSpeed * Time.deltaTime;
             GetComponent<Rigidbody2D>().MovePosition((Vector2)transform.position + (moveDir * moveSpeed * Time.deltaTime));
             // check if collided with a vertical wall, if so then change the x direction to the opposite
             // throw out raycast in the direction moving in to see if hit wall/about to hit wall
@@ -33,15 +31,19 @@ public class FlyingEnemyController : EnemyController
         RaycastHit2D downRay = Physics2D.Raycast((Vector2)transform.position + Vector2.down, Vector2.down, 0.1f);
         RaycastHit2D leftRay = Physics2D.Raycast((Vector2)transform.position + Vector2.left, Vector2.left, 0.1f);
         RaycastHit2D rightRay = Physics2D.Raycast((Vector2)transform.position + Vector2.right, Vector2.right, 0.1f);
+        // Debug.DrawRay((Vector2)transform.position + Vector2.up, Vector2.up, Color.red);
+        // Debug.DrawRay((Vector2)transform.position + Vector2.down, Vector2.down, Color.green);
+        // Debug.DrawRay((Vector2)transform.position + Vector2.left, Vector2.left, Color.yellow);
+        // Debug.DrawRay((Vector2)transform.position + Vector2.right, Vector2.right, Color.blue);
 
-        if (upRay && upRay.transform.CompareTag("Wall"))
+        if (upRay && upRay.collider.CompareTag("Wall"))
             moveDir.y = -1;
-        else if (downRay && downRay.transform.CompareTag("Wall"))
+        else if (downRay && downRay.collider.CompareTag("Wall"))
             moveDir.y = 1;
 
-        if (leftRay && leftRay.transform.CompareTag("Wall"))
+        if (leftRay && leftRay.collider.CompareTag("Wall"))
             moveDir.x = 1;
-        else if (rightRay && rightRay.transform.CompareTag("Wall"))
+        else if (rightRay && rightRay.collider.CompareTag("Wall"))
             moveDir.x = -1;
     }
 }
